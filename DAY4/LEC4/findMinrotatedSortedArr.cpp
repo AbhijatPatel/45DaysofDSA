9fd90b2070b5c83b476cde8d45cf9cd464c4578c
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2026-07-26 04:51:12 +0530

    find minimum rotated sorted array.

---
+++ DAY4/LEC4/findMinrotatedSortedArr.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMinRotateSortedArr(vector<int> & arr, int n){
+    int low = 0, high = n-1;
+    int ans = INT_MAX;
+
+    while(low <= high){
+        int mid = (low+high)/2;
+
+        if(arr[low] <= arr[mid]){
+            ans = min(ans,arr[low]);
+            low = mid +1;
+        }
+
+        else
+        {
+            ans = min(ans, arr[high]);
+            high = mid-1;
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+    vector<int> arr = {4,5,6,7,0,1,2,3};
+
+    cout << findMinRotateSortedArr(arr,arr.size());
+    return 0;
+}
