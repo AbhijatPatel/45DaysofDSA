f66f75ed728e82e899f24259192293e431cccbd2
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2026-07-31 00:44:35 +0530

    split subarray largest sum.

---
+++ DAY6/LEC6/splitArray.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPossible(vector<int> & arr, int k , int maxSum){
+    int currSum = 0;
+    k--;
+
+    for(int i=0;i<arr.size();i++){
+        if(currSum + arr[i] <= maxSum){
+            currSum += arr[i];
+        }
+
+        else {
+            currSum = arr[i];
+            k--;
+        }
+    }
+    if(k<0)
+    return false;
+
+    return true;
+}
+int splitArrLargestSum(vector<int> & arr, int k){
+    
+    int maxElm = *max_element(arr.begin(),arr.end());
+    int sum = accumulate(arr.begin(),arr.end(),0);
+
+    if(k>arr.size())
+    return -1;
+    int low = maxElm, high = sum;
+    while(low<=high){
+        int mid = (low+high)/2;
+
+        if(isPossible(arr,k,mid))
+        high = mid-1;
+
+        else
+        low = mid+1;
+    }
+    return low;
+}
+
+
+int main() {
+    vector<int> arr = {7,2,5,10,8};
+    int k = 2;
+    
+    cout<< splitArrLargestSum(arr,k); 
+    return 0;
+}
