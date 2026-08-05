c9057485b05e70f66407bf1f87d333df8cee6c16
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2026-08-06 02:59:46 +0530

    sort character by frequency.

---
+++ DAY7/LEC7/sortCharByFreq.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string frequencySort(string s){
+    map<char,int> freq;
+    for(auto ch : s)
+    freq[ch]++;
+
+    vector<vector<char>> bucketData(s.length()+1);
+    for(auto it : freq) {
+    char ch = it.first;
+    int freq = it.second;
+
+    bucketData[freq].push_back(ch);
+}
+    string ans ="";
+    for(int fq=s.length();fq>=1;fq--){
+        for(auto ch : bucketData[fq])
+        ans.append(fq,ch);
+    }
+    return ans;
+}
+int main() {
+    string s = "tree";
+
+    cout << frequencySort(s);
+    return 0;
+}
