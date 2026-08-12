ae9ff8e4a23853d671c014e13a14a5b9bdd16014
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2026-08-13 01:54:19 +0530

    detect the loop cycle in LL.

---
+++ DAY8/LEC8/detectLoopCycleLL.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+ListNode* convertArrToLL(vector<int>& arr) {
+    if(arr.empty())
+        return NULL;
+
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* mover = head;
+
+    for(int i = 1; i < arr.size(); i++) {
+        ListNode* temp = new ListNode(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+void print(ListNode* head) {
+    while(head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+
+    cout << endl;
+}
+//detect the loop cycle in LL.
+bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next !=  NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(fast == slow)
+            return true;
+        }
+        return false;
+    }
+
+int main() {
+
+    vector<int> arr = {2,3,4,5,6};
+
+    ListNode* head = convertArrToLL(arr);
+
+    // Create a cycle:
+    // 5 points back to 3
+    ListNode* temp = head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = head->next->next;
+
+    //check cycle.
+    if (hasCycle(head)) {
+        cout << "Cycle exists";
+    }
+    else {
+        cout << "Cycle does not exist";
+    }
+
+    return 0;
+}
