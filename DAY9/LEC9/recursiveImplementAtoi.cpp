68a7e11fe173f32ecd307bbf31ce307c57366047
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2026-08-15 16:40:39 +0530

    recursive implementation atoi.

---
+++ DAY9/LEC9/recursiveImplementAtoi.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fun(string s, int i, int sign, long long num) {
+    if (i >= s.size() || !isdigit(s[i]))
+        return num * sign;
+
+    num = num * 10 + (s[i] - '0');
+
+    if (num * sign > INT_MAX)
+        return INT_MAX;
+
+    if (num * sign < INT_MIN)
+        return INT_MIN;
+
+    return fun(s, i + 1, sign, num);
+}
+
+int myAtoi(string s) {
+    int n = s.size();
+    int i = 0;
+
+    while (i < n && s[i] == ' ')
+        i++;
+
+    int sign = 1;
+
+    if (i < n && s[i] == '-') {
+        sign = -1;
+        i++;
+    }
+    else if (i < n && s[i] == '+') {
+        sign = 1;
+        i++;
+    }
+
+    return fun(s, i, sign, 0);
+}
+
+int main() {
+    string s;
+
+    cout << "Enter a string: ";
+    cin>> s;
+
+    int result = myAtoi(s);
+
+    cout << "Integer: " << result << endl;
+
+    return 0;
+}
