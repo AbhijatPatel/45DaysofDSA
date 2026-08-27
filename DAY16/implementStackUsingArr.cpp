a9aa3b4f52b6c1eda4a8eafb749416179a99cd80
AbhijatPatel <abhijatpatelfaizabad@gmail.com>
2026-08-27 12:47:15 +0530

    Implementation of stack using Array.

--- DAY16/implementStackUsingArr.cpp
+++ DAY16/implementStackUsingArr.cpp
@@ -12,17 +12,31 @@ public:
     }
 
     void push(int x) {
+        if(topIndex >= 999) {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+
         topIndex++;
         arr[topIndex] = x;
     }
 
     int pop() {
+        if(topIndex == -1) {
+            cout << "Stack Underflow" << endl;
+            return -1;
+        }
+
         int x = arr[topIndex];
         topIndex--;
         return x;
     }
 
     int top() {
+        if(topIndex == -1) {
+            return -1;
+        }
+
         return arr[topIndex];
     }
 
@@ -32,17 +46,27 @@ public:
 };
 
 int main() {
+    ArrayStack st;
 
-    ArrayStack stack;
+    st.push(10);
+    st.push(20);
+    st.push(30);
 
-    stack.push(5);
-    stack.push(10);
+    cout << "Top element: " << st.top() << endl;
 
-    cout << "Top: " << stack.top() << endl;
+    cout << "Popped: " << st.pop() << endl;
 
-    cout << "Pop: " << stack.pop() << endl;
+    cout << "Top element: " << st.top() << endl;
 
-    cout << "Is Empty: " << boolalpha << stack.isEmpty() << endl;
+    cout << "Popped: " << st.pop() << endl;
+
+    cout << "Popped: " << st.pop() << endl;
+
+    if(st.isEmpty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Stack is not empty" << endl;
+    }
 
     return 0;
 }
